4f0824068d7f4e614d7a41b0e67cd4412e069fc7
imelroy <elroysonet@gmail.com>
2021-05-26 14:31:22 +0530

    successfully completed test17

--- database/migrations/2021_05_25_165215_create_laptops_table.php
+++ database/migrations/2021_05_25_165215_create_laptops_table.php
@@ -25,7 +25,8 @@ class CreateLaptopsTable extends Migration
             $table->string("Gallery");
             $table->timestamps();
         });
-        //DB::statement("ALTER TABLE laptops SEQUENCE id RESTART WITH 100;");
+        DB::statement(“ALTER SEQUENCE $table_$primarykey_seq MINVALUE 100 START 100 RESTART 100”);
+      //  DB::statement("ALTER TABLE laptops SEQUENCE id RESTART WITH 100;");
         //DB::statement("ALTER TABLE laptops AUTO_INCREMENT = 100;");
     }
 
